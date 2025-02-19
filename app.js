287a35d9a9ba2feb9471d057c965e6f392b6ac7e
omern100 <omern100@gmail.com>
2025-02-19 23:38:20 +0200

    changeAppJS

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log('JWT_SECRET:', tokenSec);
   });
 }).catch((err) => {
   console.error('Failed to connect to MongoDB:', err);
